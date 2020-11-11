9566e1dd0ba6e6cd68e5f2ac6995191cb1396dac
Svetlana <svetlankogr@gmail.com>
2020-11-11 13:41:56 +0200

    final

--- src/js/script.js
+++ src/js/script.js
@@ -88,4 +88,21 @@ $(document).ready(function(){
         return false;
     });
 
+// Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
   });
